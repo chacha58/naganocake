21f79f109c1b980bc7cfbca9193bb11532b50d37
EC2 Default User <117337820+chacha58@users.noreply.github.com>
2023-03-29 15:18:07 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  namespace :admin do
+    get 'homes/top' => ''
+    root to: 'sessions#new'
+  end
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
@@ -12,5 +16,8 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
