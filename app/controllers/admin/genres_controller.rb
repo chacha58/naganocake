8efd7fe2c6d72f78260478df7e7c1c9d8bed2404
EC2 Default User <117337820+chacha58@users.noreply.github.com>
2023-04-06 15:59:49 +0000

    Initial Commit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -17,8 +17,14 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:title)
   end
 end
